6001b9fba3e955d938b421368a89d4c7353d7c2e
Christian <35426733+cjsotoparra@users.noreply.github.com>
2018-05-31 17:27:22 -0700

    added comments, changeed JSON object name, and wrote JSON to file instead of printing
    EPO-790

--- apod_daily_fetch.py
+++ apod_daily_fetch.py
@@ -1,3 +1,17 @@
+#################################################################################
+# Author: Christian Soto                                                        #
+# Program Name: apod_daily_fetch.py                                             #
+# Email: cjsoto@lsst.org or christian.j.sotoparra@gmail.com                     #
+# Purpose: The purpose of this program is fetch the daily JSON                  #
+#          data from the website https://apod.nasa.gov/apod/astropix.html?.     #
+#          This program can be ran using python 3 and later versions.  If you   #
+#          need any earlier versions of python, be sure to import the correct   #
+#          libraries and modules.  Some of the methods from earlier modules     #
+#          might have to be changed or renamed as well.  There are currently    #
+#          no known bugs.  After the JSON data is collected, it writes it to a  #
+#          file called jsonData.txt.  If you find any bugs or need help, feel   #
+#          free to email me.                                                    #
+#################################################################################
 
 # modules needed to process internet and json data
 import urllib.request
@@ -13,20 +27,18 @@ def main():
     charset = webUrl.info().get_param('charset', 'utf8')
 
     # check if the code was able to connect to website by checking the result code.
-    # If it did, then read the data and print it, else print the error code that it returned.
+    # If it did, then read the data and write to file, else print the error code that it returned.
     if(webUrl.getcode() == 200):
 
         #read the data, the data from the website is encoded in bytes
         data = webUrl.read()
 
-        #decode the character encoding and make an JSON object called decoded
-        decoded = json.loads(data.decode(charset))
+        #decode the character encoding and make an JSON object called my_JSON_object
+        my_JSON_object = json.loads(data.decode(charset))
 
-        #here we can print fields of the JSON for example here, we are printing the field explanation, title,
-        #and date.
-        print(decoded["explanation"])
-        print(decoded["date"])
-        print(decoded["title"])
+        #create a file and write to it
+        with open("jsonData.txt", "w+") as outfile:
+            json.dump(my_JSON_object, outfile)
 
     else:
 
