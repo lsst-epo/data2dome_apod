5311cfbb3d4cb6df0579dde77f1653ed05495c58
Christian <35426733+cjsotoparra@users.noreply.github.com>
2018-06-13 11:56:23 -0700

    Working program of getting a year of JSON data
    This works on getting a years worth of JSON data from apod.  Some problems that exist though is that the way python writes the JSON data as a string and doesn't look like JSON at all.  There is a also a minor problem that this program doesn't currently check for when someone decide to start collecting on a leap year day when the year is not a leap year.

--- apod_year_round_fetch.py
+++ apod_year_round_fetch.py
@@ -45,11 +45,11 @@ def main():
 
     #This will the variables that will be needed.  For now, only the last 3 days will be gathered for simple
     #and debugging purposes
-    lastYear = date.today().year
+    lastYear = date.today().year - 1
     thisMonth = date.today().month
     thisDay = date.today().day
 
-    startDate = date(lastYear, thisMonth, thisDay - 2)
+    startDate = date(lastYear, thisMonth, thisDay)
     endDate = date.today()
 
     #This will be used to gather the JSON data and then write or append it to a file
@@ -58,7 +58,7 @@ def main():
 
         # This makes a variable that will be used to access the website in JSON format using our NASA public api key
         # Note: if you do not have an api key, then go to https://api.nasa.gov/#live_example and simply apply for one.
-        webUrl = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date="+str(dt))
+        webUrl = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=vG9rkcvYOMDKqpaVsNFSAtxwDUx376rKiQbLNIqy&date="+str(dt))
 
         # make a variable to check the character encoding
         charset = webUrl.info().get_param('charset', 'utf8')
@@ -76,7 +76,6 @@ def main():
             #create a file and write to it
             with open("jsonData.txt", "a+") as outfile:
                 json.dump(my_JSON_object, outfile)
-                print(dt)
         else:
             print("Recieved error code: " + str(webUrl.getcode()))
 
