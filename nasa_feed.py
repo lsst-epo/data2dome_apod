8aa18b8dc5d14a106148c3fcd0bb6b6bf18dc46f
Christian <35426733+cjsotoparra@users.noreply.github.com>
2018-09-06 21:48:22 -0700

    load and click more images button

--- nasa_feed.py
+++ nasa_feed.py
@@ -1,1 +1,28 @@
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+#open browser
+driver = selenium.webdriver.Firefox()
+
+#load url
+driver.get("https://www.nasa.gov/multimedia/imagegallery/iotd.html")
+
+#wait until the button loads to load more images
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "trending"))
+    )
+
+    #once the button loads, click it 16 times
+    i =0
+    while i <= 15:
+        driver.find_element_by_xpath('//*[@id="trending"]').click()
+        i += 1
+        time.sleep(1)
+finally:
+    print(i)
 
