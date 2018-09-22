4b73c5d9fdaebc1c63a067b86cce69aecc12d8b3
cjsotoparra <cjsotoparra@email.arizona.edu>
2018-09-21 22:02:45 -0700

    Trying to get 3 pictures to work

--- nasa_feed.py
+++ nasa_feed.py
@@ -37,6 +37,9 @@ def get_json(url, collection):
 	collection["Title"] = new_data["@graph"][0]["headline"]
 	collection["Credit"] = new_data["@graph"][0]["author"]["name"]
 
+	print(collection)
+
+#def getResourceImage(collection
 def getCollections():
 
 	#counter for tallying collections
@@ -45,37 +48,15 @@ def getCollections():
 	#list to store all the JSON data of collections.
 	collections = []
 
-	#collection object
-	collection = {}
-	collection["Creator"] = "Nasa image of the Day"
-	collection["URL"] = "https://nasa.gov"
-	#collection["ReferenceURL"] = "https://nasa.gov/multimedia/imagegallery/iotd.html"
-
-	#new dictionary for contact information
-	contact_JSON = {}
-	contact_JSON["Name"] ="Robert Nemiroff"
-	contact_JSON["Email"] = "nemiroff@mtu.edu"
-	contact_JSON["Telephone"] = "1-906-487-2198"
-	contact_JSON["Address"] = "1400 Townsend Drive"
-	contact_JSON["City"] = "Houghton"
-	contact_JSON["StateProvince"] = "Michigan"
-	contact_JSON["PostalCode"] = "49931"
-	contact_JSON["Country"] = "USA"
-
-	#add contact_JSON object to collection Object
-	collection["Contact"] = contact_JSON
-
-	#List to store all the JSON data of Assets
-	#asset={}
+	#options for headless mode
+	options = Options()
+	options.set_headless(headless=True)
 
 	#open browser
-	driver = selenium.webdriver.Firefox(executable_path=r'./geckodriver')
+	driver = selenium.webdriver.Firefox(firefox_options=options,executable_path=r'./geckodriver')
 
-	#options = Options();
+	#make url varaible to hold url
 	url = "https://www.nasa.gov/multimedia/imagegallery/iotd.html"
-
-	#add url to collection object
-	collection["ReferenceURL"] = url
 
 	#load url
 	driver.get(url)
@@ -95,38 +76,69 @@ def getCollections():
 	#click on the first card or picture
 	driver.find_element_by_class_name('gallery-card:first-child').click()
 
-	#get the hreh link for current page
-	date_url = driver.find_element_by_partial_link_text('View Image')
-	description = driver.find_element_by_tag_name('p')
-	collection["Description"] = description.text
+	i = 0
+	while i<= 3:
 
-	#get the rest of the image collection
-	get_json(date_url.get_attribute("href"), collection)
+		#get the hreh link for current page
+		date_url = driver.find_element_by_partial_link_text('View Image')
 
-	#except:
- 	#  	print("Some kind of error exception")
+		#collection object
+		collection = {}
+		collection["Creator"] = "Nasa image of the Day"
+		collection["URL"] = "https://nasa.gov"
+		collection["ReferenceURL"] = url
 
+		#new dictionary for contact information
+		contact_JSON = {}
+		contact_JSON["Name"] ="Robert Nemiroff"
+		contact_JSON["Email"] = "nemiroff@mtu.edu"
+		contact_JSON["Telephone"] = "1-906-487-2198"
+		contact_JSON["Address"] = "1400 Townsend Drive"
+		contact_JSON["City"] = "Houghton"
+		contact_JSON["StateProvince"] = "Michigan"
+		contact_JSON["PostalCode"] = "49931"
+		contact_JSON["Country"] = "USA"
+
+		#add contact_JSON object to collection Object
+		collection["Contact"] = contact_JSON
+
+		#get the description
+		description = driver.find_element_by_tag_name('p')
+		collection["Description"] = description.text
+
+		#get the rest of the image collection
+		get_json(date_url.get_attribute("href"), collection)
+
+		#add current collection JSON object to collections list
+		collections.append(collection)
+
+		#after collectiong, click on the next button for next picture
+		driver.find_element_by_class_name('flex-next').click()
+
+		i+=1
+
+	#close the driver
 	driver.close()
 
 	return{"collections":collections, "count":counter}
 
 def main():
 
-    #This dictionary will collect all the JSON data and stored here
-    main_JSON = {}
+	#This dictionary will collect all the JSON data and stored here
+	main_JSON = {}
 
-    #Get APOD data for last 365 days
-    nasa_data = getCollections()
+	#Get APOD data for last 365 days
+	nasa_data = getCollections()
+	print(nasa_data)
+	#This list will collect contact information and assets
+	#main_JSON["Collections"] = nasa_data["collections"]
 
-    #This list will collect contact information and assets
-    #main_JSON["Collections"] = nasa_data["collections"]
+	#Number sources where JSON was collected
+	#main_JSON["Count"] = nasa_data["count"]
 
-    #Number sources where JSON was collected
-    #main_JSON["Count"] = nasa_data["count"]
-
-    #This creates a new file where JSON data will be stored
-   # with open("apod_v2.json", "w+") as outfile:
-    #   json.dump(main_JSON, outfile)
+	#This creates a new file where JSON data will be stored
+	# with open("apod_v2.json", "w+") as outfile:
+	#   json.dump(main_JSON, outfile)
 
 #Run main
 if __name__ == "__main__":
